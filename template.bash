073d3097634f431f6aa71129cd58a20659150d2f
curiositycasualty <if_then@hey.com>
2021-06-15 16:24:16 -0700

    linting config

---
+++ template.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Copyright:: 2021, LabKey Inc., All Rights Reserved.
+
+if [ -n "${DEBUG:-}" ]; then
+  set -x
+fi
+
+# bash strict mode
+set -euo pipefail
+
+function main() {
+
+  true
+
+}
+
+main
