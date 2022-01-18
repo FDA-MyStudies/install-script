8e137f0fa59c8209c8945114c383df1be2191cc7
Stuart MacDonald <32204841+labkey-stuartm@users.noreply.github.com>
2022-01-18 14:38:50 -0800

    update to adoptium java 17

--- sample_embedded_envs.sh
+++ sample_embedded_envs.sh
@@ -12,7 +12,7 @@ export LABKEY_BASE_SERVER_URL="https://localhost"
 #export LABKEY_INSTALL_SKIP_START_LABKEY_STEP=1
 export POSTGRES_SVR_LOCAL="TRUE"
 
-export LABKEY_DIST_URL="https://lk-binaries.s3.us-west-2.amazonaws.com/downloads/release/community/21.7.0/LabKey21.7.0-2-community-embedded.tar.gz"
-export LABKEY_DIST_FILENAME="LabKey21.7.0-2-community-embedded.tar.gz"
-export LABKEY_VERSION="21.7.0"
+export LABKEY_DIST_URL="https://lk-binaries.s3.us-west-2.amazonaws.com/downloads/release/community/21.11.5/LabKey21.11.5-6-community-embedded.tar.gz"
+export LABKEY_DIST_FILENAME="LabKey21.11.5-6-community-embedded.tar.gz"
+export LABKEY_VERSION="21.11.5"
 export LABKEY_DISTRIBUTION="community"
